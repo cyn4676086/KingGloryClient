3d7e41160c8d22848e2ba35e44dcfd99ca18012a
cyn4676086 <4676086@qq.com>
2022-04-19 15:26:02 +0800

    BUG修复与优化： 新增英雄属性增长机制 玩家收益Buff机制 修复伤害为负导致的炮塔不能连续攻击的八阿哥 修复小兵无法攻击炮塔目标的八阿哥 修复炮塔红线位置异常的八阿哥 其他若干BUG修复

---
+++ Assets/Scripts/BirthWater.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirthWater : MonoBehaviour {
+    
+    public int group;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerModel>() == null)
+        {
+            return;
+        }
+        if(other.GetComponent<PlayerModel>().Group==group&& other.GetComponent<PlayerModel>().isDead != true)
+        {
+            cor= StartCoroutine(Cure(other));
+        }
+    }
+    private Coroutine cor;
+    private void OnTriggerExit(Collider other)
+    {
+        if (cor != null)
+        {
+            StopCoroutine(cor);
+        }
+        
+    }
+    
+    private IEnumerator Cure(Collider other)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            if(other.GetComponent<PlayerModel>().HP < other.GetComponent<HealthBar>().defaultHealth-1)
+            {
+                BattleFieldRequest.Instance.HurtRequest(other.GetComponent<PlayerModel>().id, 1500, other.GetComponent<PlayerModel>().id);
+            }
+
+            if (other.GetComponent<PlayerModel>().HP ==other.GetComponent<HealthBar>().defaultHealth - 1)
+            {
+                StopCoroutine(cor);
+                break;
+            }
+        }
+    }
+}
